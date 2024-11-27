d08ac2ebf24f7c18daaca2c19872c9f29a708a82
Alejandro Tolomei <atolomei@novamens.com>
2024-11-27 09:21:09 -0300

    schedule

--- src/main/java/io/odilon/scheduler/SchedulerWorker.java
+++ src/main/java/io/odilon/scheduler/SchedulerWorker.java
@@ -46,204 +46,197 @@ import io.odilon.virtualFileSystem.model.VirtualFileSystemService;
  */
 public abstract class SchedulerWorker implements Runnable {
 
-	static private Logger logger = Logger.getLogger(SchedulerWorker.class.getName());
-	static private Logger startuplogger = Logger.getLogger("StartupLogger");
+    static private Logger logger = Logger.getLogger(SchedulerWorker.class.getName());
+    static private Logger startuplogger = Logger.getLogger("StartupLogger");
 
-	static public final long ONE_SECOND = 1000;
-	static public final long _SIESTA_SECS = 40;
-	static public final long _STARTUP_SIESTA_MILSECS = 15 * 1000;
+    static public final long ONE_SECOND = 1000;
+    static public final long _SIESTA_SECS = 40;
+    static public final long _STARTUP_SIESTA_MILSECS = 15 * 1000;
 
-	private String id;
+    private String id;
 
-	private int poolSize = 1;
+    private int poolSize = 1;
 
-	@JsonIgnore
-	private AtomicBoolean running = new AtomicBoolean(false);
+    @JsonIgnore
+    private AtomicBoolean running = new AtomicBoolean(false);
 
-	@JsonIgnore
-	private long siesta_mili = _SIESTA_SECS * 1000;
+    @JsonIgnore
+    private long siesta_mili = _SIESTA_SECS * 1000;
 
-	@JsonIgnore
-	private Thread thread;
+    @JsonIgnore
+    private Thread thread;
 
-	@JsonIgnore
-	private OffsetDateTime created = OffsetDateTime.now();
+    @JsonIgnore
+    private OffsetDateTime created = OffsetDateTime.now();
 
-	@JsonIgnore
-	private Dispatcher dispatcher;
+    @JsonIgnore
+    private Dispatcher dispatcher;
 
-	@JsonIgnore
-	private VirtualFileSystemService virtualFileSystemService;
+    @JsonIgnore
+    private VirtualFileSystemService virtualFileSystemService;
 
-	@JsonIgnore
-	private Map<Serializable, ServiceRequest> executingRequests = new HashMap<Serializable, ServiceRequest>();
+    @JsonIgnore
+    private Map<Serializable, ServiceRequest> executingRequests = new HashMap<Serializable, ServiceRequest>();
 
-	@JsonIgnore
-	private List<ServiceRequest> failedRequests = new ArrayList<ServiceRequest>();
+    @JsonIgnore
+    private List<ServiceRequest> failedRequests = new ArrayList<ServiceRequest>();
 
-	@JsonIgnore
-	private ApplicationContext applicationContext;
+    @JsonIgnore
+    private ApplicationContext applicationContext;
 
-	/** only used by the SchedulerWorker Thread */
-	@JsonIgnore
-	private volatile boolean sleeping = false;
+    /** only used by the SchedulerWorker Thread */
+    @JsonIgnore
+    private volatile boolean sleeping = false;
 
-	/** methods to add and terminate ServiceRequests (close, fail, cancel) */
-	public abstract void fail(ServiceRequest request);
+    /** methods to add and terminate ServiceRequests (close, fail, cancel) */
+    public abstract void fail(ServiceRequest request);
 
-	public abstract void cancel(ServiceRequest request);
+    public abstract void cancel(ServiceRequest request);
 
-	public abstract void close(ServiceRequest request);
+    public abstract void close(ServiceRequest request);
 
-	public abstract void add(ServiceRequest request);
+    public abstract void add(ServiceRequest request);
 
-	/** ------ */
+    protected abstract void onInitialize();
 
-	protected abstract void onInitialize();
+    protected abstract void doJobs();
 
-	protected abstract void doJobs();
+    protected abstract void restNoWork();
 
-	protected abstract void restNoWork();
+    protected abstract void restFullCapacity();
 
-	protected abstract void restFullCapacity();
+    protected abstract boolean isWork();
 
-	protected abstract boolean isWork();
+    protected abstract boolean isFullCapacity();
 
-	protected abstract boolean isFullCapacity();
+    public SchedulerWorker(String id, VirtualFileSystemService virtualFileSystemService) {
+        this.id = id;
+        this.virtualFileSystemService = virtualFileSystemService;
+    }
 
-	public SchedulerWorker(String id, VirtualFileSystemService virtualFileSystemService) {
-		this.id = id;
-		this.virtualFileSystemService = virtualFileSystemService;
-	}
+    public void start() {
 
-	/**
-	 * 						
-	 */
-	public void start() {
+        this.poolSize = getPoolSize();
 
-		this.poolSize = getPoolSize();
+        setSiestaMillisecs(getVFS().getServerSettings().getSchedulerSiestaSecs() * 1000);
 
-		setSiestaMillisecs(getVFS().getServerSettings().getSchedulerSiestaSecs() * 1000);
+        this.dispatcher = new Dispatcher(getId(), 1, this.poolSize);
+        this.thread = new Thread(this);
+        this.thread.setDaemon(true);
+        this.thread.setName(this.getClass().getSimpleName() + "_" + getId());
+        this.thread.setPriority(1);
 
-		this.dispatcher = new Dispatcher(getId(), 1, this.poolSize);
-		this.thread = new Thread(this);
-		this.thread.setDaemon(true);
-		this.thread.setName(this.getClass().getSimpleName() + "_" + getId());
-		this.thread.setPriority(1);
+        onInitialize();
 
-		onInitialize();
+        this.thread.start();
 
-		this.thread.start();
+        startuplogger.debug("Started -> " + this.getClass().getName());
+    }
 
-		startuplogger.debug("Started -> " + this.getClass().getName());
+    public int getPoolSize() {
+        return getVFS().getServerSettings().getDispatcherPoolSize();
+    }
 
-	}
+    @Override
+    public void run() {
 
-	public int getPoolSize() {
-		return getVFS().getServerSettings().getDispatcherPoolSize();
-	}
+        rest(_STARTUP_SIESTA_MILSECS);
 
-	@Override
-	public void run() {
+        this.running = new AtomicBoolean(true);
 
-		rest(_STARTUP_SIESTA_MILSECS);
+        while (isRunning()) {
 
-		this.running = new AtomicBoolean(true);
+            try {
 
-		while (isRunning()) {
+                doJobs();
 
-			try {
+                if (isFullCapacity())
+                    restFullCapacity();
 
-				doJobs();
+                if (!isWork())
+                    restNoWork();
 
-				if (isFullCapacity())
-					restFullCapacity();
+            } catch (Throwable e) {
+                processError(e);
+            }
+        }
 
-				if (!isWork())
-					restNoWork();
+        rest(_STARTUP_SIESTA_MILSECS);
 
-			} catch (Throwable e) {
-				processError(e);
-			}
-		}
+        logger.error("ending main loop");
 
-		rest(_STARTUP_SIESTA_MILSECS);
+        this.running = new AtomicBoolean(false);
 
-		logger.error("ending main loop");
+    }
 
-		this.running = new AtomicBoolean(false);
+    public void stop() {
+        this.running = new AtomicBoolean(false);
+    }
 
-	}
+    public OffsetDateTime getStarted() {
+        return this.created;
+    }
 
-	public void stop() {
-		this.running = new AtomicBoolean(false);
-	}
+    public ApplicationContext getApplicationContext() {
+        return this.applicationContext;
+    }
 
-	public OffsetDateTime getStarted() {
-		return this.created;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public ApplicationContext getApplicationContext() {
-		return this.applicationContext;
-	}
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public VirtualFileSystemService getVFS() {
+        return this.virtualFileSystemService;
+    }
 
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+    protected void setSiestaMillisecs(long siestaMill) {
+        this.siesta_mili = siestaMill;
+    }
 
-	public VirtualFileSystemService getVFS() {
-		return this.virtualFileSystemService;
-	}
+    protected long getSiestaMillisecs() {
+        return this.siesta_mili;
+    }
 
-	protected void setSiestaMillisecs(long siestaMill) {
-		this.siesta_mili = siestaMill;
-	}
+    protected void rest(long value) {
+        try {
+            if (value > 0) {
+                synchronized (this) {
+                    setSleeping(true);
+                    this.wait(value);
+                    setSleeping(false);
+                }
+            }
+        } catch (InterruptedException e) {
+            setSleeping(false);
+        } catch (Throwable e) {
+            logger.error(e, SharedConstant.NOT_THROWN);
+            setSleeping(false);
+        }
+    }
 
-	protected long getSiestaMillisecs() {
-		return this.siesta_mili;
-	}
+    protected void dispatch(ServiceRequest job) {
+        job.setApplicationContext(getApplicationContext());
+        ServiceRequestExecutor executor = new ServiceRequestExecutor(job, this);
+        getDispatcher().dispatch(executor);
+    }
 
-	protected void rest(long value) {
-		try {
-			if (value > 0) {
-				synchronized (this) {
-					setSleeping(true);
-					this.wait(value);
-					setSleeping(false);
-				}
-			}
-		} catch (InterruptedException e) {
-			setSleeping(false);
-		} catch (Throwable e) {
-			logger.error(e, SharedConstant.NOT_THROWN);
-			setSleeping(false);
-		}
-	}
+    protected Dispatcher getDispatcher() {
+        return this.dispatcher;
+    }
 
-	protected void dispatch(ServiceRequest job) {
-		job.setApplicationContext(getApplicationContext());
-		ServiceRequestExecutor executor = new ServiceRequestExecutor(job, this);
-		getDispatcher().dispatch(executor);
-	}
+    protected void processError(Throwable e) {
+        logger.error(e, SharedConstant.NOT_THROWN);
+    }
 
-	protected Dispatcher getDispatcher() {
-		return this.dispatcher;
-	}
+    private void setSleeping(boolean value) {
+        this.sleeping = value;
+    }
 
-	protected void processError(Throwable e) {
-		logger.error(e, SharedConstant.NOT_THROWN);
-	}
-
-	private void setSleeping(boolean value) {
-		this.sleeping = value;
-	}
-
-	private boolean isRunning() {
-		return this.running.get();
-	}
-
+    private boolean isRunning() {
+        return this.running.get();
+    }
 }
